2d0533b495efbc8439dfce97dd3b551e4e2d7bc1
Eskil Kvalnes <eskil@eskil-mbp.local>
2011-09-28 04:13:06 +0200

    bumped version number

--- libraries/class.hub.php
+++ libraries/class.hub.php
@@ -3,7 +3,7 @@ require_once APP_PATH.'/libraries/api.thetvdb.php';
 require_once APP_PATH.'/libraries/api.boxcar.php';
 
 class Hub {
-	const HubVersion   = '2.2.11';
+	const HubVersion   = '2.3.0';
 	const MinDBVersion = '2.0.0';
 	
 	public $PDO;

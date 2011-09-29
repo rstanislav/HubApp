rstanislav/HubApp

commit 2986c36acf3050c19b953477fbc5f0b550e0e46b

changed format for season/episode on boxcar notification

libraries/class.extract-files.php
@@ -223,7 +223,7 @@ class ExtractFiles extends Hub {
 							                                  ':Season'  => $ParsedEpisode[0],
 							                                  ':Episode' => $ParsedEpisode[1]));
 							                                  
-							Hub::NotifyUsers('NewLibraryEpisode', 'XBMC/Series', '"'.$SerieTitle.' s'.sprintf('%02s', $ParsedEpisode[0]).'e'.sprintf('%02s', $ParsedEpisode[1]).'" is now available on "'.$DriveRoot.'"');
+							Hub::NotifyUsers('NewLibraryEpisode', 'XBMC/Series', '"'.$SerieTitle.' '.$ParsedEpisode[0]).'x'.$ParsedEpisode[1].'" is now available on "'.$DriveRoot.'"');
 						}
 					}
 				}

commit 6a4fb3d219d0414f4cad53a30c2a7c703978784d

fix for parse error caused by 2986c36acf3050c19b953477fbc5f0b550e0e46b

libraries/class.extract-files.php
@@ -223,7 +223,7 @@ class ExtractFiles extends Hub {
 							                                  ':Season'  => $ParsedEpisode[0],
 							                                  ':Episode' => $ParsedEpisode[1]));
 							                                  
-							Hub::NotifyUsers('NewLibraryEpisode', 'XBMC/Series', '"'.$SerieTitle.' '.$ParsedEpisode[0]).'x'.$ParsedEpisode[1].'" is now available on "'.$DriveRoot.'"');
+							Hub::NotifyUsers('NewLibraryEpisode', 'XBMC/Series', '"'.$SerieTitle.' '.$ParsedEpisode[0].'x'.$ParsedEpisode[1].'" is now available on "'.$DriveRoot.'"');
 						}
 					}
 				}
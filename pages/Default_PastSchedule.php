fb276a2848c0dabf60c6fe084be45042eb1647d0
Eskil Kvalnes <eskil@eskil-mbp.local>
2011-10-20 00:13:31 +0200

    fixed wrong variable in torrent download link

--- pages/Default_PastSchedule.php
+++ pages/Default_PastSchedule.php
@@ -30,7 +30,7 @@ if($Series) {
 			$FileAction = ($UserObj->CheckPermission($UserObj->UserGroupID, 'XBMCPlay')) ? '<a id="FilePlay-'.$Serie['EpisodeFile'].'"><img src="images/icons/control_play.png" title="Play '.$Serie['EpisodeFile'].'" /></a>' : '';
 		}
 		else if($Serie['TorrentKey']) {
-			$FileAction = '<a id="DownloadTorrent-'.$Serie['EpisodeID'].'-'.$RSSTorrents[0]['TorrentID'].'"><img src="images/icons/downloaded.png" title="Episode has been added to uTorrent. Click to re-download" /></a>';
+			$FileAction = '<a id="DownloadTorrent-'.$Serie['EpisodeID'].'-'.$Serie['TorrentKey'].'"><img src="images/icons/downloaded.png" title="Episode has been added to uTorrent. Click to re-download" /></a>';
 		}
 		else if($RSSTorrents) {
 			if(sizeof($RSSTorrents) > 1) {

8f5c3e7493be082fa6c2e244f9947d49d6d2bdfb
Eskil Kvalnes <eskil.kvalnes@gmail.com>
2012-10-30 01:06:27 +0100

    added bookmarklet for adding movies to wishlist

---
+++ bookmarklet.php
@@ -0,0 +1,8 @@
+<script src="js/hub.bookmarklet.js"></script>
+
+<a href="javascript:(function(){
+  _my_script=document.createElement('SCRIPT');
+  _my_script.type='text/javascript';
+  _my_script.src='http://ip/js/hub.bookmarklet.js?x='+(Math.random());
+  document.getElementsByTagName('head')[0].appendChild(_my_script);
+})();">bookmarklet</a>

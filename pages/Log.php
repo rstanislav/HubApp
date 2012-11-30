631c713704a1ca1b235786a649901f7215847dcc
Eskil Kvalnes <eskil.kvalnes@gmail.com>
2012-11-30 03:56:58 +0100

    added grouping of log messages

--- pages/Log.php
+++ pages/Log.php
@@ -16,13 +16,44 @@ else {
 	  <th>Text</th>
 	 </tr>
 	 </thead>'."\n";
-	
-	foreach($Logs AS $Log) {
+
+	for($i = 0; $i < sizeof($Logs); $i++) {
+		if($Logs[$i]->Event == 'RSS') {
+			preg_match('/(Added) ([0-9]+) (torrents spread across) ([0-9]+) (RSS feeds)/', $Logs[$i]->Text, $LogPrevious);
+			
+			$TorrentsAdded = $LogPrevious[2];
+			$RSSFeeds      = $LogPrevious[4];
+			for($k = ($i + 1); $k < sizeof($Logs); $k++) {
+				if($Logs[$k]->Event == 'RSS') {
+					preg_match('/(Added) ([0-9]+) (torrents spread across) ([0-9]+) (RSS feeds)/', $Logs[$k]->Text, $LogNew);
+				
+					$ToDate         = $Logs[$i]->Date;
+					$TorrentsAdded += $LogNew[2];
+					$RSSFeeds       = ($LogNew[4] > $RSSFeeds) ? $LogNew[4] : $RSSFeeds;
+					continue;
+				}
+				else {
+					$i = ($k - 1);
+					break;
+				}
+			}
+		}
+		else {
+			$TorrentsAdded = 0;
+			$ToDate = 0;
+		}
+		
+		if($TorrentsAdded) {
+			$Logs[$i]->Text = 'Added '.$TorrentsAdded.' torrents spread across '.$RSSFeeds.' RSS feeds';
+		}
+		
+		$LogDateTo = !empty($ToDate) ? date('-d.m H:i', $ToDate) : '';
+		
 		echo '
 		<tr>
-		 <td>'.date('d.m H:i', $Log->Date).'</td>
-		 <td>'.$Log->Event.'</td>
-		 <td>'.nl2br($Log->Text).'</td>
+		 <td>'.date('d.m H:i', $Logs[$i]->Date).$LogDateTo.'</td>
+		 <td>'.$Logs[$i]->Event.'</td>
+		 <td>'.$Logs[$i]->Text.'</td>
 		</tr>'."\n";
 	}
 	
